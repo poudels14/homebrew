5fcb4a5b5350466ef5e2d7bec359bdff66bf1743
Sagar Poudel <poudels14@gmail.com>
2015-07-04 19:17:57 -0400

    Added torch7 for luajit

---
+++ Library/Formula/torch7.rb
@@ -0,0 +1,32 @@
+# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                /usr/local/Library/Contributions/example-formula.rb
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Torch7 < Formula
+  desc "Torch package for lua"
+  homepage "http://torch.ch/"
+  url "https://github.com/UPenn-RoboCup/torch7.git"
+  version "7"
+  sha256 "asdasd"
+
+	depends_on "lua5.1.5"
+
+  def install
+		system "make", "prep"
+		system "make", "-j4"
+		system "make", "install"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test torch`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
